317ceddf331ef40670a836360abad27c478daae3
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2018-03-25 14:14:53 +0800

    Jni LeafLet

---
+++ java/src/main/java/com/github/ng8eke/fareast/NativeMethods.java
@@ -0,0 +1,31 @@
+package com.github.ng8eke.Fareast;
+
+// jni: javac -h . src\main\java\com\github\ng8eke\Fareast\NativeMethods.java
+class NativeMethods
+{
+    native static long loadIndex(String path, int dimension, byte type);
+
+    native static void freeIndex(long pointer);
+
+    native static float[] getItemVector(long indexPointer, long itemIndex);
+
+    native static int getNearestToItem(
+            long indexPointer,
+            long itemIndex, 
+            int nResult, 
+            int searchK, 
+            boolean shouldIncludeDistance,
+            long[] idList,
+            float[] distanceList
+    );
+        
+    native static int getNearest(
+            long indexPointer,
+            float[] queryVector, 
+            int nResult, 
+            int searchK, 
+            boolean shouldIncludeDistance,
+            long[] idList,
+            float[] distanceList
+        );
+}
